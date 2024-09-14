bed8459bd5b04b0dec735f6fa7c4e84de73daa3d
sonjuhyeon <ths120521113105@daum.net>
2024-09-14 16:31:33 +0900

    saved domain in .env

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,12 +1,9 @@
-const GET_TASKS_API_URL = "https://taskback.microdeveloper.co.kr/get_tasks";
-const POST_TASKS_API_URL = "https://taskback.microdeveloper.co.kr/post_task";
-const DELETE_TASKS_API_URL =
-  "https://taskback.microdeveloper.co.kr/delete_task";
-const UPDATE_COMPLETED_TASKS_API_URL =
-  "https://taskback.microdeveloper.co.kr/update_completed_task";
-const UPDATE_IMPORTANT_TASKS_API_URL =
-  "https://taskback.microdeveloper.co.kr/update_important_task";
-const UPDATE_TASK_API_URL = "https://taskback.microdeveloper.co.kr/update_task";
+const GET_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_tasks`;
+const POST_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/post_task`;
+const DELETE_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/delete_task`;
+const UPDATE_COMPLETED_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_completed_task`;
+const UPDATE_IMPORTANT_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_important_task`;
+const UPDATE_TASK_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_task`;
 
 export {
   GET_TASKS_API_URL,
